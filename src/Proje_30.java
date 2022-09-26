5953a2502708ee33876f05e74bc272dba4e69fae
huseyin <hsyn_hsyn77@hotmail.com>
2022-09-26 18:27:49 +0300

    fibonacci

--- src/Proje_30.java
+++ src/Proje_30.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 public class Proje_30 {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Lütfen eleman sayısını giriniz : ");
+        Scanner s = new Scanner(System.in);
 
-        int sayi = scan.nextInt();
-        int n1 = 0, n2 = 1;
-        System.out.print(n1 + " " + n2 + " ");
-        for (int i = 1; i <= (sayi - 2); i++) {
-            int result = n1 + n2;
-            System.out.print(result + " ");
-           n1 = n2;
-           n2 = result;
+        System.out.print("Sayı Giriniz : ");
+        int n = s.nextInt();
+        int a = 1;
+        int b = 1;
+        int c;
+        System.out.print("0");
+        System.out.print(""+a);
+        System.out.print(""+b);
+        for(int i=0; i<n-3; i++){
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.print(""+c);
         }
     }
 }
