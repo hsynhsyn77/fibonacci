66f8c6730dd1abe89b4f5a4c9302ce644139b1ef
huseyin <hsyn_hsyn77@hotmail.com>
2022-09-26 18:25:03 +0300

    Initial commit

---
+++ src/Proje_30.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Proje_30 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lütfen eleman sayısını giriniz : ");
+
+        int sayi = scan.nextInt();
+        int n1 = 0, n2 = 1;
+        System.out.print(n1 + " " + n2 + " ");
+        for (int i = 1; i <= (sayi - 2); i++) {
+            int result = n1 + n2;
+            System.out.print(result + " ");
+           n1 = n2;
+           n2 = result;
+        }
+    }
+}
